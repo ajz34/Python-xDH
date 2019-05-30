travis: try xyg3 gradient; make a commit first, since b2plyp at least correct

pyxdhalpha/DerivOnce/deriv_once_mp2.py
@@ -1,12 +1,12 @@
 import numpy as np
-from abc import ABC
+from abc import ABC, abstractmethod
 from functools import partial
 import os
 import warnings
 
 from pyscf.scf import cphf
 
-from pyxdhalpha.DerivOnce import DerivOnce
+from pyxdhalpha.DerivOnce import DerivOnce, DerivOnceNCDFT
 
 MAXMEM = float(os.getenv("MAXMEM", 2))
 np.einsum = partial(np.einsum, optimize=["greedy", 1024 ** 3 * MAXMEM / 8])
@@ -15,8 +15,10 @@ np.set_printoptions(8, linewidth=1000, suppress=True)
 
 class DerivOnceMP2(DerivOnce, ABC):
 
-    def __init__(self, scf_eng, rotation=True, grdit_memory=2000):
+    def __init__(self, scf_eng, rotation=True, grdit_memory=2000, cc=1.0):
         super(DerivOnceMP2, self).__init__(scf_eng, rotation, grdit_memory)
+        self.cc = cc
+
         self._t_iajb = NotImplemented
         self._T_iajb = NotImplemented
         self._L = NotImplemented
@@ -73,7 +75,7 @@ class DerivOnceMP2(DerivOnce, ABC):
         return self.eri0_mo[so, sv, so, sv] / self.D_iajb
 
     def _get_T_iajb(self):
-        return 2 * self.t_iajb - self.t_iajb.swapaxes(-1, -3)
+        return self.cc * (2 * self.t_iajb - self.t_iajb.swapaxes(-1, -3))
 
     def _get_L(self):
         nvir, nocc, nmo = self.nvir, self.nocc, self.nmo
@@ -134,3 +136,21 @@ class DerivOnceMP2(DerivOnce, ABC):
         )
 
     # endregion
+
+
+class DerivOnceXDH(DerivOnceMP2, ABC):
+
+    def __init__(self, scf_eng, nc_eng, rotation=True, grdit_memory=2000, cc=1.):
+        super(DerivOnceXDH, self).__init__(scf_eng, rotation, grdit_memory, cc)
+        self.nc_deriv = self.DerivOnceNCGGAClass(nc_eng, self.C, self.mo_occ,
+                                                 rotation, grdit_memory)  # type: DerivOnceNCDFT
+
+    @property
+    @abstractmethod
+    def DerivOnceNCGGAClass(self):
+        pass
+
+    def _get_L(self):
+        L = super(DerivOnceXDH, self)._get_L()
+        L += self.nc_deriv.F_0_mo[self.sv, self.so]
+        return L

pyxdhalpha/DerivOnce/grad_mp2.py
@@ -2,14 +2,14 @@ import numpy as np
 from functools import partial
 import os
 
-from pyxdhalpha.DerivOnce import DerivOnceMP2, GradSCF
+from pyxdhalpha.DerivOnce import DerivOnceMP2, DerivOnceXDH, GradSCF, GradNCDFT
 
 MAXMEM = float(os.getenv("MAXMEM", 2))
 np.einsum = partial(np.einsum, optimize=["greedy", 1024 ** 3 * MAXMEM / 8])
 np.set_printoptions(8, linewidth=1000, suppress=True)
 
 
-class GradMP2(GradSCF, DerivOnceMP2):
+class GradMP2(DerivOnceMP2, GradSCF):
 
     def Ax1_Core(self, si, sj, sk, sl):
         raise NotImplementedError("This is still under construction...")
@@ -23,6 +23,27 @@ class GradMP2(GradSCF, DerivOnceMP2):
             + 2 * np.einsum("iajb, Aiajb -> A", self.T_iajb, self.eri1_mo[:, so, sv, so, sv])
         ).reshape(natm, 3)
         E_1 += super(GradMP2, self)._get_E_1()
+        return E_1
+
+
+class GradXDH(DerivOnceXDH, GradMP2):
+
+    def Ax1_Core(self, si, sj, sk, sl):
+        raise NotImplementedError("This is still under construction...")
+
+    @property
+    def DerivOnceNCGGAClass(self):
+        return GradNCDFT
+
+    def _get_E_1(self):
+        so, sv = self.so, self.sv
+        natm = self.natm
+        E_1 = (
+            + np.einsum("pq, Apq -> A", self.D_r, self.B_1)
+            + np.einsum("pq, Apq -> A", self.W_I, self.S_1_mo)
+            + 2 * np.einsum("iajb, Aiajb -> A", self.T_iajb, self.eri1_mo[:, so, sv, so, sv])
+        ).reshape(natm, 3)
+        E_1 += self.nc_deriv._get_E_1()
         return E_1
 
 
@@ -54,4 +75,46 @@ class Test_GradMP2:
         assert(np.allclose(
             gmh.E_1, formchk.grad(),
             atol=1e-6, rtol=1e-4
+        ))
+
+    def test_B2PLYP_grad(self):
+
+        from pkg_resources import resource_filename
+        from pyxdhalpha.Utilities.test_molecules import Mol_H2O2
+        from pyxdhalpha.Utilities import FormchkInterface
+
+        H2O2 = Mol_H2O2(xc="0.53*HF + 0.47*B88, 0.73*LYP")
+        gmh = GradMP2(H2O2.gga_eng, cc=0.27)
+
+        formchk = FormchkInterface(resource_filename("pyxdhalpha", "Validation/gaussian/H2O2-B2PLYP-freq.fchk"))
+
+        assert(np.allclose(
+            gmh.scf_eng.e_tot + (gmh.T_iajb * gmh.t_iajb * gmh.D_iajb).sum(),
+            formchk.total_energy()
+        ))
+        assert(np.allclose(
+            gmh.E_1, formchk.grad(),
+            atol=1e-5, rtol=1e-4
+        ))
+
+    def test_XYG3_grad(self):
+
+        from pkg_resources import resource_filename
+        from pyxdhalpha.Utilities.test_molecules import Mol_H2O2
+        from pyxdhalpha.Utilities import FormchkInterface
+
+        H2O2_sc = Mol_H2O2(xc="B3LYPg")
+        H2O2_nc = Mol_H2O2(xc="0.8033*HF - 0.0140*LDA + 0.2107*B88, 0.6789*LYP")
+        gmh = GradXDH(H2O2_sc.gga_eng, H2O2_nc.gga_eng, cc=0.3211)
+
+        formchk = FormchkInterface(resource_filename("pyxdhalpha", "Validation/gaussian/H2O2-XYG3-force.fchk"))
+
+        assert(np.allclose(
+            + gmh.nc_deriv.scf_eng.energy_tot(dm=gmh.D)
+            + (gmh.T_iajb * gmh.t_iajb * gmh.D_iajb).sum(),
+            formchk.total_energy()
+        ))
+        assert(np.allclose(
+            gmh.E_1, formchk.grad(),
+            atol=1e-5, rtol=1e-4
         ))

pyxdhalpha/DerivOnce/grad_scf.py
@@ -4,7 +4,7 @@ import os
 
 from pyscf import grad
 
-from pyxdhalpha.DerivOnce.deriv_once import DerivOnce
+from pyxdhalpha.DerivOnce.deriv_once import DerivOnce, DerivOnceNCDFT
 from pyxdhalpha.Utilities import GridIterator, KernelHelper
 
 MAXMEM = float(os.getenv("MAXMEM", 2))
@@ -52,24 +52,22 @@ class GradSCF(DerivOnce):
         return eri1_ao.reshape(-1, self.nao, self.nao, self.nao, self.nao)
     
     def _get_E_1(self):
+        cx, xc = self.cx, self.xc
+        so, sv = self.so, self.sv
+        mol, natm = self.mol, self.natm
         D = self.D
         H_1_ao = self.H_1_ao
         eri1_ao = self.eri1_ao
-        S_1_ao = self.S_1_ao
-        Co = self.Co
-        eo = self.eo
-        mol = self.mol
-        natm = self.natm
+        S_1_mo = self.S_1_mo
+        F_0_mo = self.F_0_mo
         grids = self.grids
         grdit_memory = self.grdit_memory
-        cx = self.cx
-        xc = self.xc
 
         grad_total = (
             + np.einsum("Auv, uv -> A", H_1_ao, D)
             + 0.5 * np.einsum("Auvkl, uv, kl -> A", eri1_ao, D, D)
             - 0.25 * cx * np.einsum("Aukvl, uv, kl -> A", eri1_ao, D, D)
-            - 2 * np.einsum("Auv, ui, i, vi -> A", S_1_ao, Co, eo, Co)
+            - 2 * np.einsum("Aij, ij -> A", S_1_mo[:, so, so], F_0_mo[so, so])
             + grad.rhf.grad_nuc(mol).reshape(-1)
         )
 
@@ -84,6 +82,15 @@ class GradSCF(DerivOnce):
                 ).reshape(-1)
 
         return grad_total.reshape(natm, 3)
+
+
+class GradNCDFT(DerivOnceNCDFT, GradSCF):
+
+    def Ax1_Core(self, si, sj, sk, sl):
+        raise NotImplementedError("This is still under construction...")
+
+    def _get_E_1(self):
+        raise NotImplementedError("This is still under construction...")
 
 
 class Test_GradSCF: