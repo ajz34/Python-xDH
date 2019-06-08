a33b016b6d52feab355a54f26a4083c0c1ac4960
ajz34 <17110220038@fudan.edu.cn>
2019-06-08 16:10:52 +0800

    pyxdhalpha: minor add to __init__.py

--- pyxdhalpha/DerivTwice/__init__.py
+++ pyxdhalpha/DerivTwice/__init__.py
@@ -3,3 +3,4 @@ from pyxdhalpha.DerivTwice.deriv_twice_mp2 import DerivTwiceMP2, DerivTwiceXDH
 from pyxdhalpha.DerivTwice.hess_scf import HessSCF, HessNCDFT
 from pyxdhalpha.DerivTwice.hess_mp2 import HessMP2, HessXDH
 from pyxdhalpha.DerivTwice.polar_scf import PolarSCF, PolarNCDFT
+from pyxdhalpha.DerivTwice.polar_mp2 import PolarMP2, PolarXDH
